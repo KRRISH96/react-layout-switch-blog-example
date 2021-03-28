98cb5e370c514a25841296f814f23d8a514140e2
sai <kskprasad96@gmail.com>
2021-03-28 12:50:20 +0530

    wireup the LayoutSwitch and children in App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,71 @@
 import React from 'react';
+import { useFetch } from '../hooks/useFetch';
+import UsersTable from './UsersTable';
+import LayoutSwitch from './LayoutSwitch';
+import UsersGrid from './UsersGrid';
+import { LAYOUT_OPTIONS } from '../constants';
+import './App.scss';
+
+export interface UserData {
+  id: number;
+  name: string;
+  username: string;
+  company: Company;
+}
+
+interface Company {
+  id: number;
+  name: string;
+}
+
+const layoutOptionValues = Object.values(LAYOUT_OPTIONS);
+
+export interface UsersListProps {
+  users: UserData[];
+  activeLayout: typeof layoutOptionValues[number];
+}
 
 function App() {
-  return <div className="App">Hello There!</div>;
+  const {
+    response: { data: users },
+    error,
+    loading,
+  } = useFetch<UserData[]>('/users');
+
+  if (error) {
+    return <h2 className="error">{error}</h2>;
+  }
+
+  return (
+    <div className="container app">
+      <h2>Users</h2>
+      {loading && <h3>Loading Users...</h3>}
+      {users !== null ? (
+        <LayoutSwitch defaultLayout={LAYOUT_OPTIONS.grid}>
+          <LayoutSwitch.Options>
+            <LayoutSwitch.Button
+              layoutPreference={LAYOUT_OPTIONS.table}
+              title="Table Layout"
+            >
+              <p>Hello Table</p>
+            </LayoutSwitch.Button>
+            <LayoutSwitch.Button
+              layoutPreference={LAYOUT_OPTIONS.grid}
+              title="Grid Layout"
+            >
+              <p>Hello Grid</p>
+            </LayoutSwitch.Button>
+          </LayoutSwitch.Options>
+          <LayoutSwitch.Content>
+            <UsersTable activeLayout={LAYOUT_OPTIONS.table} users={users} />
+            <UsersGrid activeLayout={LAYOUT_OPTIONS.grid} users={users} />
+          </LayoutSwitch.Content>
+        </LayoutSwitch>
+      ) : (
+        <h3>No Users Found</h3>
+      )}
+    </div>
+  );
 }
 
 export default App;
